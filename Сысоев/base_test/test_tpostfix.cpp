55e0abc45917199a08e488744cd9dce5d3ef176f
AUXLander <auxlander.1000@gmail.com>
2018-11-04 21:05:36 +0300

    renamed

--- Сысоев/base_test/test_tpostfix.cpp
+++ Сысоев/base_test/test_tpostfix.cpp
@@ -73,13 +73,13 @@ TEST(TPostfix, throw_if_expression_has_non_number_member) {
 	ASSERT_ANY_THROW(TPostfix a("5 + 4 - 3 + a"));
 }
 
-TEST(TPostfix, exeption) {
+TEST(TPostfix, can_transform_multi_dividing) {
 	TPostfix a("1+2/4/5-5");
 	EXPECT_EQ("1 2 4 5*/ -5++", a.GetPostfix());
 	EXPECT_EQ(-3.9000000000000004, a.Calculate());// Есть некоторая погрешность
 }
 
-TEST(TPostfix, exeption_add) {
+TEST(TPostfix, can_transform_multi_dividing_add) {
 	TPostfix a("100/2/2/5/5");
 	EXPECT_EQ(1, a.Calculate());
 }
